c74854a23b2d22cda2f1dd39ec9a12139410bfe3
shahidFladdra <shahid.bangi@fladdra.com>
2021-10-02 18:33:16 +0530

    fonts added

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,4 +3,5 @@ class Routes {
   static String signup1Route = "/signup1";
   static String signup2Route = "/signup2";
   static String signup3Route = "/signup3";
+  static String signupRoute = "/signup";
 }
