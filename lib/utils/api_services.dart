d3a4cf225355b856db98f7eaf3010dfa062827c7
shahidFladdra <shahid.bangi@fladdra.com>
2021-12-17 16:24:54 +0530

    url updates

--- lib/utils/api_services.dart
+++ lib/utils/api_services.dart
@@ -6,7 +6,7 @@ class ApiServices {
   final String baseUrlLive = "http://202.53.174.5:8000";
   final String baseUrlLocal = "http://192.168.0.194:5500";
   String get baseUri => baseUriImage + "/";
-  String get baseUriImage => baseUrlLocal;
+  String get baseUriImage => baseUrlLive;
 
   Uri restaurants(String path) => Uri.parse(baseUri + "restaurants/" + path);
 
